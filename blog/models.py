61b5ff7465c3b2c40bb02ac0ff4f466fdd675a99
Leonardo Costa <l.j.m.costa@hotmail.com>
2024-08-20 13:37:49 +0100

    Getting started with the models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
+from django.utils.text import slugify
 # Create your models here.
+
+
+class Post(models.Model):
+    pass
+
+
+class Author(models.Model):
+    pass
+
+
+class Tag(models.Model):
+    pass
