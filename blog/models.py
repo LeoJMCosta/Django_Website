ed74003a5fc067055cdabb70ebb18b4c8d12f7d5
Leonardo Costa <l.j.m.costa@hotmail.com>
2024-08-21 14:55:23 +0100

    Corrected the models + took out comments

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,13 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
-from django.urls import reverse
-from django.utils.text import slugify
+from django.core.validators import MinLengthValidator
 # Create your models here.
 
 
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
 
-    # class Meta:
-    #     verbose_name_plural = "Tags"
+    def __str__(self):
+        return self.caption
 
 
 class Author(models.Model):
@@ -17,11 +15,11 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     email_adddress = models.EmailField()
 
-    # def full_name(self):
-    #     return f"{self.first_name} {self.last_name}"
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
 
-    # class Meta:
-    #     verbose_name_plural = "Authors"
+    def __str__(self):
+        return self.full_name()
 
 
 class Post(models.Model):
@@ -30,16 +28,7 @@ class Post(models.Model):
     image = models.CharField(max_length=100)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
-    content = models.TextField(validators=[MinValueValidator(10)])
+    content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(
-        Author, on_delete=models.SET_NULL, related_name="posts")
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags = models.ManyToManyField(Tag)
-
-    # def get_absolute_url(self):
-    #     return reverse("book_detail", args=[self.slug])
-
-    # def __str__(self):
-    #     return f"{self.title} ({self.author})"
-
-    # class Meta:
-    #     verbose_name_plural = "Posts"
