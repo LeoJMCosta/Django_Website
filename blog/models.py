a80a324bd24934593c724df04c26f55732c11a8a
Leonardo Costa <l.j.m.costa@hotmail.com>
2024-08-21 14:25:26 +0100

    Changed the models a bit

--- blog/models.py
+++ blog/models.py
@@ -5,43 +5,41 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    # class Meta:
+    #     verbose_name_plural = "Tags"
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email_adddress = models.EmailField(_(""), max_length=254)
+    email_adddress = models.EmailField()
 
-    def full_name(self):
-        return f"{self.first_name} {self.last_name}"
+    # def full_name(self):
+    #     return f"{self.first_name} {self.last_name}"
 
-    class Meta:
-        verbose_name_plural = "Authors"
+    # class Meta:
+    #     verbose_name_plural = "Authors"
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image = ""
-    date = ""
-    slug = models.SlugField(default="", blank=True,
-                            null=False, db_index=True)
+    image = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinValueValidator(10)])
     author = models.ForeignKey(
-        Author, on_delete=models.CASCADE, null=True, related_name="posts")
-    content = models.CharField
+        Author, on_delete=models.SET_NULL, related_name="posts")
+    tags = models.ManyToManyField(Tag)
 
-    def get_absolute_url(self):
-        return reverse("book_detail", args=[self.slug])
+    # def get_absolute_url(self):
+    #     return reverse("book_detail", args=[self.slug])
 
-    def __str__(self):
-        return f"{self.title} ({self.author})"
+    # def __str__(self):
+    #     return f"{self.title} ({self.author})"
 
-    class Meta:
-        verbose_name_plural = "Posts"
-
-
-class Tag(models.Model):
-    caption = models.CharField(max_length=100)
-
-    post_tags = models.ManyToManyField(Post, null=False)
-
-    class Meta:
-        verbose_name_plural = "Tags"
+    # class Meta:
+    #     verbose_name_plural = "Posts"
